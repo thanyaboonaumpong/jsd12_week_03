a8d676646813f429dc3c0b8607461cdd60544425
thanyaboonaumpong <thanyaboonaumpong@hotmail.com>
2026-03-20 08:04:00 +0700

    js-functions

---
+++ js-functions/arrow-functions.js
@@ -0,0 +1,12 @@
+// arrow function was introduced in ES6, ECMAScript, ES6 => ECMMAScript 2015
+
+// anonymous arrow function
+() => {};
+
+// arrow function
+const greetUser = (name) => {;
+
+    greetUser("Neeti");
+
+    console.log(greetUser("Neeti"));
+}
